dda82140f88a97fa7b3397dcf3a9f8e146df975e
stan wielga <svwielga4@gmail.com>
2019-09-15 19:29:49 -0700

    adding board class

--- goboard_slow.py
+++ goboard_slow.py
@@ -21,3 +21,101 @@ class Move():
     @classmethod
     def resign(cls):
         return Move(is_resign=True)
+
+
+# GoString is a group of pieces on the board, tracking groups of pieces as a unit
+# greatly decreases the amount of calculation needed
+
+
+class GoString():
+    def __init__(self, color, stones, liberities):
+        self.color = color
+        self.stones = stones
+        self.liberities = liberities
+
+    def remove_liberty(self, point):
+        self.liberities.remove(point)
+
+    def add_liberty(self, point):
+        self.liberities.add(point)
+
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberities | go_string.liberities) - combined_stones
+        )
+
+    @property
+    def num_liberities(self):
+        return len(self.liberities)
+
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberities == other.liberities
+
+
+class Board():
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {}
+
+    def place_stone(self, player, point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberities = []
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_string = self._grid.get(neighbor)
+            if neighbor_string is None:
+                liberities.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+        new_string = GoString(player, [point], liberities)
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
+
+    def is_on_grid(self, point):
+        return 1 <= point.row <= self.num_rows and \
+            1 <= point.col <= self.num_cols
+
+        def get(self, point):
+            string = self._grid.get(point)
+            if string is None:
+                return None
+            return string.color
+
+        def get_go_string(self, point):
+            string = self._grid.get(point)
+            if string is None:
+                return None
+            return string
+
+    def _remove_string(self, string):
+        for point in string.stones:
+            for neighbor in point.neighbors():
+                neighbor_string = self._grid.get(neighbor)
+                if neighbor_string is None:
+                    continue
+                if neighbor_string is not string:
+                    neighbor_string.add_liberty(point)
+            self._grid[point] = None
