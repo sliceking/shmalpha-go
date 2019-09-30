911fbc1af19e4f06d979af5886b5aa26666f7b4d
svwielga4 <svwielga4@gmail.com>
2019-09-30 15:50:42 -0700

    mcts

---
+++ dlgo/mcts/mcts.py
@@ -0,0 +1,72 @@
+import math
+import random
+
+from dlgo import agent
+from dlgo.gotypes import Player
+from dlgo.utils import coords_from_point
+
+
+def fmt(x):
+    if x is Player.black:
+        return 'B'
+    if x is Player.white:
+        return 'W'
+    if x.is_pass:
+        return 'pass'
+    if x.is_resign:
+        return 'resign'
+    return coords_from_point(x.point)
+
+
+def show_tree(node, indent='', max_depth=3):
+    if max_depth < 0:
+        return
+    if node is None:
+        return
+    if node.parent is None:
+        print('%sroot' % indent)
+    else:
+        player = node.parent.game_state.next_player
+        move = node.move
+        print('%s%s %s %d %.3f' % (
+            indent, fmt(player), fmt(move),
+            node.num_rollouts,
+            node.winning_frac(player),
+        ))
+    for child in sorted(node.children, key=lambda n: n.num_rollouts, reverse=True):
+        show_tree(child, indent + '  ', max_depth - 1)
+
+
+class MCTSNode(object):
+    def __init__(self, game_state, parent=None, move=None):
+        self.game_state = game_state
+        self.parent = parent
+        self.move = move
+        self.win_counts = {
+            Player.black: 0,
+            Player.white: 0,
+        }
+        self.num_rollouts = 0
+        self.children = []
+        self.unvisited_moves = game_state.legal_moves()
+
+    def add_random_child(self):
+        index = random.randint(0, len(self.unvisited_moves) - 1)
+        new_move = self.unvisited_moves.pop(index)
+        new_game_state = self.game_state.apply_move(new_move)
+        new_node = MCTSNode(new_game_state, self, new_move)
+        self.children.append(new_node)
+        return new_node
+
+    def record_win(self, winner):
+        self.win_counts[winner] += 1
+        self.num_rollouts += 1
+
+    def can_add_child(self):
+        return len(self.unvisited_moves) > 0
+
+    def is_terminal(self):
+        return self.game_state.is_over()
+
+    def winning_frac(self, player):
+        return float(self.win_counts[player]) / float(self.num_rollouts)
