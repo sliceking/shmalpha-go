d37a6d688a7ca0a3a0867c44d3bec4909b441187
stan wielga <svwielga4@gmail.com>
2019-09-30 17:33:12 -0700

    mcts

--- dlgo/mcts/mcts.py
+++ dlgo/mcts/mcts.py
@@ -70,3 +70,84 @@ class MCTSNode(object):
 
     def winning_frac(self, player):
         return float(self.win_counts[player]) / float(self.num_rollouts)
+
+
+class MCTSAgent(agent.Agent):
+    def __init__(self, num_rounds, temperature):
+        agent.Agent.__init__(self)
+        self.num_rounds = num_rounds
+        self.temperature = temperature
+
+    def select_move(self, game_state):
+        root = MCTSNode(game_state)
+
+        for i in range(self.num_rounds):
+            node = root
+            while (not node.can_add_child()) and (not node.is_terminal()):
+                node = self.select_child(node)
+
+                # add a new child node to the tree
+                if node.can_add_child():
+                    node.add_random_child()
+
+                # simulate a random game from this node
+                winner = self.simulate_random_game(node.game_state)
+
+                # propagate scores back up the tree
+                while node is not None:
+                    node.record_win(winner)
+                    node = node.parent
+
+        scored_moves = [
+            (child.winning_frac(game_state.next_player),
+             child.move, child.num_rollouts)
+            for child in root.children
+        ]
+
+        scored_moves.sort(key=lambda x: x[0], reverse=True)
+        for s, m, n in scored_moves[:10]:
+            print('%s - %.3f (%d)' % (m, s, n))
+
+        # having performed as many MCTS rounds as we have time for,
+        # we can now pick a move
+        best_move = None
+        best_pct = -1.0
+        for child in root.children:
+            child_pct = child.winning_frac(game_state.next_player)
+            if child_pct > best_pct:
+                best_pct = child_pct
+                best_move = child.move
+        print('Select move %s with win pct %.3f' % (best_move, best_pct))
+        return best_move
+
+    def select_child(self, node):
+        """Select a child according to the upper confidence bound for trees
+        (UCT) metric
+        """
+        total_rollouts = sum(child.num_rollouts for child in node.children)
+        log_rollouts = math.log(total_rollouts)
+
+        best_score = -1
+        best_child = None
+        # Loop over each child
+        for child in node.children:
+            # calculate the UCT score
+            win_percentage = child.winning_frac(node.game_state.next_player)
+            exploration_factor = math.sqrt(log_rollouts / child.num_rollouts)
+            uct_score = win_percentage + self.temperature * exploration_factor
+            # Check if this is the largest we've seen so far
+            if uct_score > best_score:
+                best_score = uct_score
+                best_child = child
+        return best_child
+
+    @staticmethod
+    def simulate_random_game(game):
+        bots = {
+            Player.black: agent.FastRandomBot(),
+            Player.white: agent.FastRandomBot(),
+        }
+        while not game.is_over():
+            bot_move = bots[game.next_player].select_move(game)
+            game = game.apply_move(bot_move)
+        return game.winner()
