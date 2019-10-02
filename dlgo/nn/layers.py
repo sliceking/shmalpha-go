11485dac7a12ca0e540233e43653778446dc86d4
svwielga4 <svwielga4@gmail.com>
2019-10-02 13:14:27 -0700

    added dense layer

--- dlgo/nn/layers.py
+++ dlgo/nn/layers.py
@@ -99,3 +99,58 @@ class ActivationLayer(Layer):
         print("|-- " + self.__class__.__name__)
         print("  |-- dimensions: ({},{})"
               .format(self.input_dim, self.output_dim))
+
+
+class DenseLayer(Layer):
+    def __init__(self, input_dim, output_dim):
+        # dense layers have input and output dimensions
+        super(DenseLayer, self).__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        # We randomly initialize weight matrix and bias vector.
+        self.weight = np.random.randn(output_dim, input_dim)
+        self.bias = np.random.randn(output_dim, 1)
+
+        # the layer parameters consist of weights and bias terms
+        self.params = [self.weight, self.bias]
+
+        # deltas for weights and biases are set to zero
+        self.delta_w = np.zeros(self.weight.shape)
+        self.delta_b = np.zeros(self.bias.shape)
+
+    def forward(self):
+        data = self.get_forward_input()
+        # the forward pass of the dense layer is the affine linear transformation on input data defined by weights and biases
+        self.output_data = np.dot(self.weight, data) + self.bias
+
+    def backward(self):
+        data = self.get_forward_input()
+        # for the backward pass we first get the input data and delta
+        delta = self.get_backward_input()
+
+        # the current delta is added to teh bias delta
+        self.delta_b += delta
+
+        # then we add this term to the weight delta
+        self.delta_w += np.dot(delta, data.transpose())
+
+        # the backward pass is completed bu passing an output delta to the previous layer
+        self.output_delta = np.dot(self.weight.transpose(), delta)
+
+    def update_params(self, rate):
+        # using weight and bias deltas we can update model parameters with gradient descent
+        self.weight -= rate * self.delta_w
+        self.bias -= rate * self.delta_b
+
+    def clear_deltas(self):
+        # after updating parameters we should reset all deltas
+        self.delta_w = np.zeros(self.weight.shape)
+        self.delta_b = np.zeros(self.bias.shape)
+
+    def describe(self):
+        # a dense layer can be described by its input and output dimensions
+        print("|---" + self.__class__.__name__)
+        print(" |-- dimensions: ({},{})"
+              .format(self.input_dim, self.output_dim))
